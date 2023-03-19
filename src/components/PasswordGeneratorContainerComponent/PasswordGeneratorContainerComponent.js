f75fbe244c0e3a113135dc3b7c807268119f2c60
phillipsantos <116766285+butterybagels@users.noreply.github.com>
2023-03-19 15:27:01 -0400

    Added logic to handle complexity option selections and update state

--- src/components/PasswordGeneratorContainerComponent/PasswordGeneratorContainerComponent.js
+++ src/components/PasswordGeneratorContainerComponent/PasswordGeneratorContainerComponent.js
@@ -4,6 +4,14 @@ import './PasswordGenerator.css'
 export default function PasswordGeneratorContainerComponent() {
 
     const [generatedPassword, setGeneratedPassword] = useState("")
+    const [complexityOptions, setComplexityOptions] = useState({
+        length: 12,
+        useUppercase: false,
+        useLowercase: false,
+        useNumbers: false,
+        useSpecialCharacters: false
+    })
+
     function handleGenerate() {
         console.log('Generating...')
     }
@@ -22,6 +30,41 @@ export default function PasswordGeneratorContainerComponent() {
 
     function includeSpecialCharacter() {
         return '!@#$%^&*()'
+    }
+
+    function handleLength(e) {
+        setComplexityOptions((prevState) => ({
+            ...prevState,
+                length: e.target.value
+        }))
+    }
+
+    function handleUppercaseSelected(e) {
+        setComplexityOptions((prevState) => ({
+            ...prevState,
+            useUppercase: e.target.checked
+        }))
+    }
+
+    function handleLowercaseSelected(e) {
+        setComplexityOptions((prevState) => ({
+            ...prevState,
+            useLowercase: e.target.checked
+        }))
+    }
+
+    function handleNumbersSelected(e) {
+        setComplexityOptions((prevState) => ({
+            ...prevState,
+            useNumbers: e.target.checked
+        }))
+    }
+
+    function handleSpecialCharactersSelected(e) {
+        setComplexityOptions((prevState) => ({
+            ...prevState,
+            useSpecialCharacters: e.target.checked
+        }))
     }
 
     function generatePassword(e, desiredLength, useUppercase, useLowercase, useNumbers, useSpecialCharacters) {
@@ -45,39 +88,38 @@ export default function PasswordGeneratorContainerComponent() {
             generatedPassword += passwordCharacter
         }
 
-        console.log(generatedPassword)
-        return generatedPassword
+        setGeneratedPassword(generatedPassword)
     }
 
     return (
         <div className="container">
             <div className="generator-container">
                 <form>
-                    <input type="text" value={generatedPassword} onChange={handleGenerate} disabled={true}/>
+                    <input type="text" value={generatedPassword} disabled={true}/>
                     <button className="btn btn-info">Copy</button><br />
-                    <input className="form-range" type="range" min="4" max="20" step="1" defaultValue="12"/>
+                    <input className="form-range" type="range" min="4" max="20" step="1" defaultValue={complexityOptions.length} onChange={(e) => handleLength(e)}/>
 
                     <div className="form-check form-switch">
                         {/*<input className="form-check-input" type="checkbox" id="use-uppercase"/>*/}
-                        <label className="form-check-label" htmlFor="use-uppercase">Use uppercase letters <input className="form-check-input" type="checkbox" id="use-uppercase" onChange={handleUppercaseSelected}/></label>
+                        <label className="form-check-label" htmlFor="use-uppercase">Use uppercase letters <input className="form-check-input" type="checkbox" id="use-uppercase" onChange={(e) => handleUppercaseSelected(e)}/></label>
                     </div>
 
                     <div className="form-check form-switch">
                         {/*<input className="form-check-input" type="checkbox" id="use-lowercase"/>*/}
-                        <label className="form-check-label" htmlFor="use-lowercase">Use lowercase letters <input className="form-check-input" type="checkbox" id="use-lowercase" onChange={handleLowercaseSelected}/></label>
+                        <label className="form-check-label" htmlFor="use-lowercase">Use lowercase letters <input className="form-check-input" type="checkbox" id="use-lowercase" onChange={(e) => handleLowercaseSelected(e)}/></label>
                     </div>
 
                     <div className="form-check form-switch">
                         {/*<input className="form-check-input" type="checkbox" id="use-numbers"/>*/}
-                        <label className="form-check-label" htmlFor="use-numbers">Use numbers <input className="form-check-input" type="checkbox" id="use-numbers" onChange={handleNumbersSelected}/></label>
+                        <label className="form-check-label" htmlFor="use-numbers">Use numbers <input className="form-check-input" type="checkbox" id="use-numbers" onChange={(e) => handleNumbersSelected(e)}/></label>
                     </div>
 
                     <div className="form-check form-switch">
                         {/*<input className="form-check-input" type="checkbox" id="use-special-characters"/>*/}
-                        <label className="form-check-label" htmlFor="use-special-characters">Use special characters <input className="form-check-input" type="checkbox" id="use-special-characters" onChange={handleSpecialCharactersSelected}/></label>
+                        <label className="form-check-label" htmlFor="use-special-characters">Use special characters <input className="form-check-input" type="checkbox" id="use-special-characters" onChange={(e) => handleSpecialCharactersSelected(e)}/></label>
                     </div>
 
-                    <button className="btn btn-info" onClick={(e) => generatePassword(e, 20, false, true, false, true)}>Generate</button>
+                    <button className="btn btn-info" onClick={(e) => generatePassword(e, complexityOptions.length, complexityOptions.useUppercase, complexityOptions.useLowercase, complexityOptions.useNumbers, complexityOptions.useSpecialCharacters)}>Generate</button>
                 </form>
             </div>
         </div>
