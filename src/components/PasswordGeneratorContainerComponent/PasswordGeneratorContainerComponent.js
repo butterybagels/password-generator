b48761bf6d63c59fee24db8c043cbde7bf3ad5c6
phillipsantos <116766285+butterybagels@users.noreply.github.com>
2023-03-19 15:27:01 -0400

    Minor function name refactors

--- src/components/PasswordGeneratorContainerComponent/PasswordGeneratorContainerComponent.js
+++ src/components/PasswordGeneratorContainerComponent/PasswordGeneratorContainerComponent.js
@@ -8,32 +8,20 @@ export default function PasswordGeneratorContainerComponent() {
         console.log('Generating...')
     }
 
-    function generateUppercase() {
-        const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-        return uppercaseLetters
-        // return uppercaseLetters.charAt(Math.floor(Math.random() * uppercaseLetters.length))
+    function includeUppercase() {
+        return 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     }
 
-    function generateLowercase() {
-        const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz'
-
-        return lowercaseLetters
-        // return lowercaseLetters.charAt(Math.floor(Math.random() * lowercaseLetters.length))
+    function includeLowercase() {
+        return 'abcdefghijklmnopqrstuvwxyz'
     }
 
-    function generateNumber() {
-        const numbers = '0123456789'
-
-        return numbers
-        // return numbers.charAt(Math.floor(Math.random() * numbers.length))
+    function includeNumber() {
+        return '0123456789'
     }
 
-    function generateSpecialCharacter() {
-        const specialCharacters = '!@#$%^&*()'
-
-        return specialCharacters
-        // return specialCharacters.charAt(Math.floor(Math.random() * specialCharacters.length))
+    function includeSpecialCharacter() {
+        return '!@#$%^&*()'
     }
 
     function generatePassword(e, desiredLength, useUppercase, useLowercase, useNumbers, useSpecialCharacters) {
@@ -43,49 +31,22 @@ export default function PasswordGeneratorContainerComponent() {
         let generatedPassword = ""
         let complexityCharacters = ""
 
-        if (useUppercase) complexityCharacters += generateUppercase()
+        if (useUppercase) complexityCharacters += includeUppercase()
 
-        if (useLowercase) complexityCharacters += generateLowercase()
+        if (useLowercase) complexityCharacters += includeLowercase()
 
-        if (useNumbers) complexityCharacters += generateNumber()
+        if (useNumbers) complexityCharacters += includeNumber()
 
-        if (useSpecialCharacters) complexityCharacters += generateSpecialCharacter()
+        if (useSpecialCharacters) complexityCharacters += includeSpecialCharacter()
 
-
-        // Loop through desired length
         for (let i = 0; i < desiredLength; i++) {
-            // let randomCharacter = Math.floor(Math.random(4))
-
             let passwordCharacter = complexityCharacters.charAt(Math.floor(Math.random() * complexityCharacters.length))
 
             generatedPassword += passwordCharacter
-            // switch (randomCharacter) {
-            //     case 0:
-            //         let uppercaseLetter = generateUppercase()
-            //         generatedPassword += uppercaseLetter
-            //         break;
-            //     case 1:
-            //         let lowercaseLetter = generateLowercase()
-            //         generatedPassword += lowercaseLetter
-            //         break;
-            //     case 2:
-            //         let number = generateNumber()
-            //         generatedPassword += number
-            //         break;
-            //     case 3:
-            //         let specialCharacter = generateSpecialCharacter()
-            //         generatedPassword += specialCharacter
-            //         break;
-            //     default:
-            //         break;
-            // }
         }
 
         console.log(generatedPassword)
         return generatedPassword
-        // Choose randomly from letter, number or character
-        // Append to generated password string
-        // Return generated password string
     }
 
     return (
@@ -98,22 +59,22 @@ export default function PasswordGeneratorContainerComponent() {
 
                     <div className="form-check form-switch">
                         {/*<input className="form-check-input" type="checkbox" id="use-uppercase"/>*/}
-                        <label className="form-check-label" htmlFor="use-uppercase">Use uppercase letters <input className="form-check-input" type="checkbox" id="use-uppercase"/></label>
+                        <label className="form-check-label" htmlFor="use-uppercase">Use uppercase letters <input className="form-check-input" type="checkbox" id="use-uppercase" onChange={handleUppercaseSelected}/></label>
                     </div>
 
                     <div className="form-check form-switch">
                         {/*<input className="form-check-input" type="checkbox" id="use-lowercase"/>*/}
-                        <label className="form-check-label" htmlFor="use-lowercase">Use lowercase letters <input className="form-check-input" type="checkbox" id="use-lowercase"/></label>
+                        <label className="form-check-label" htmlFor="use-lowercase">Use lowercase letters <input className="form-check-input" type="checkbox" id="use-lowercase" onChange={handleLowercaseSelected}/></label>
                     </div>
 
                     <div className="form-check form-switch">
                         {/*<input className="form-check-input" type="checkbox" id="use-numbers"/>*/}
-                        <label className="form-check-label" htmlFor="use-numbers">Use numbers <input className="form-check-input" type="checkbox" id="use-numbers"/></label>
+                        <label className="form-check-label" htmlFor="use-numbers">Use numbers <input className="form-check-input" type="checkbox" id="use-numbers" onChange={handleNumbersSelected}/></label>
                     </div>
 
                     <div className="form-check form-switch">
                         {/*<input className="form-check-input" type="checkbox" id="use-special-characters"/>*/}
-                        <label className="form-check-label" htmlFor="use-special-characters">Use special characters <input className="form-check-input" type="checkbox" id="use-special-characters"/></label>
+                        <label className="form-check-label" htmlFor="use-special-characters">Use special characters <input className="form-check-input" type="checkbox" id="use-special-characters" onChange={handleSpecialCharactersSelected}/></label>
                     </div>
 
                     <button className="btn btn-info" onClick={(e) => generatePassword(e, 20, false, true, false, true)}>Generate</button>
