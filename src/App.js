0c6d9775377fd15bcc0b928854617a9abb2ff102
phillipsantos <116766285+butterybagels@users.noreply.github.com>
2023-03-19 15:27:01 -0400

    Removed Create-React-App boilerplate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import PasswordGeneratorContainerComponent
+    from "./components/PasswordGeneratorContainerComponent/PasswordGeneratorContainerComponent";
 
 function App() {
   return (
     <div className="App">
-      <h1>This is the password generator.</h1>
+        <h1>This is the password generator.</h1>
+        <PasswordGeneratorContainerComponent />
     </div>
   );
 }
---
+++ src/components/PasswordGeneratorContainerComponent/PasswordGeneratorContainerComponent.js
@@ -0,0 +1,46 @@
+import {useState} from "react";
+
+export default function PasswordGeneratorContainerComponent() {
+
+    const [generatedPassword, setGeneratedPassword] = useState("")
+    function handleGenerate() {
+        console.log('Generating...')
+    }
+
+    return (
+        <div className="container">
+            <form>
+                <input type="text" value={generatedPassword} onChange={handleGenerate} disabled={true}/>
+                <button className="btn btn-info">Copy</button><br />
+                <input className="form-range" type="range" min="4" max="20" step="1" defaultValue="12"/>
+
+                <div className="form-check">
+                    <label className="form-check-label" htmlFor="use-uppercase">Use uppercase letters</label>
+                    <input className="form-check-input" type="checkbox" id="use-uppercase"/>
+                </div>
+
+                <div className="form-check">
+                    <label className="form-check-label" htmlFor="use-lowercase">Use lowercase letters</label>
+                    <input className="form-check-input" type="checkbox" id="use-lowercase"/>
+                </div>
+
+                <div className="form-check">
+                    <label className="form-check-label" htmlFor="use-numbers">Use numbers</label>
+                    <input className="form-check-input" type="checkbox" id="use-numbers"/>
+                </div>
+
+                <div className="form-check">
+                    <label className="form-check-label" htmlFor="use-special-characters">Use special characters</label>
+                    <input className="form-check-input" type="checkbox" id="use-special-characters"/>
+                </div>
+
+                {/*<label>Use lowercase letters</label>*/}
+                {/*<input type="checkbox"/><br/>*/}
+                {/*<label>Use numbers</label>*/}
+                {/*<input type="checkbox"/><br/>*/}
+                {/*<label>Use special characters</label>*/}
+                {/*<input type="checkbox"/>*/}
+            </form>
+        </div>
+    )
+}
